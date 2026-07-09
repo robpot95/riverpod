046de49547a0a5789fe20029db8756f20d1be4d2
Roberto Potrus <roberto_potrus@hotmail.com>
2026-07-09 12:57:14 +0200

    update

--- packages/riverpod/lib/src/core/provider/notifier_provider.dart
+++ packages/riverpod/lib/src/core/provider/notifier_provider.dart
@@ -257,6 +257,10 @@ extension NotifierPersistX<StateT, ValueT> on AnyNotifier<StateT, ValueT> {
         // Let's read the Database
         final futureOr = storage.then(
           (storage) => storage.read(key).then((value) {
+            if (!ref.mounted) {
+              return null;
+            }
+
             // The state was initialized during the decoding, abort
             if (didChange) return null;
             // Nothing to decode
