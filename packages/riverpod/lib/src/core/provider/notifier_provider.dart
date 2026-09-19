617a0afb40fc8107bdaaae0f8f32124b51d9e2df
Roberto Potrus <roberto_potrus@hotmail.com>
2026-09-19 23:38:11 +0200

    update

--- packages/riverpod/lib/src/core/provider/notifier_provider.dart
+++ packages/riverpod/lib/src/core/provider/notifier_provider.dart
@@ -302,10 +302,10 @@ extension NotifierPersistX<StateT, ValueT> on AnyNotifier<StateT, ValueT> {
 abstract class $AsyncNotifierBase<ValueT> extends AnyNotifier<AsyncValue<ValueT>, ValueT> {
   @override
   void _setStateFromValue(ValueT value) {
-    if (state is! AsyncData) {
+    /* if (state is! AsyncData) {
       state = AsyncLoading._((progress: state.progress), value: (value, kind: DataKind.cache, source: DataSource.liveOrRefresh), error: state._error);
-    }
-    //state = AsyncData._((value, kind: DataKind.cache, source: DataSource.liveOrRefresh), error: state._error, loading: (progress: state.progress));
+    }*/
+    state = AsyncData._((value, kind: DataKind.cache, source: DataSource.liveOrRefresh), error: state._error, loading: (progress: state.progress));
   }
 
   @override
